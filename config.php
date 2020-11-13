d30ce936d93e6d2f32eff9511b5c66b6d7caf3be
Abeneshks <ksabenesh@gmail.com>
2020-11-13 16:34:06 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 //Api Telegram, replace "XXXXXXX:XXXXXXXXXXXXXXXXXXXXX" with your API KEY
-define('BOT_TOKEN', 'XXXXXXX:XXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '1264762370:AAHNV8bXSQ21WMb7bdx5bRRowzDEgSV4WQI');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 //Bot id, change with your bot id
-define('BOT_ID', '@botname');
-define('REF_AMZ','');
+define('BOT_ID', '@Dealsbyksa_bot');
+define('REF_AMZ','abitrickzword-21');
 ?>
